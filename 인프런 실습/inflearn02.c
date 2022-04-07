6250688dd522746184599549c767946e97a8b8ff
Chae Haram <djvemfla2@gmail.com>
2022-04-08 01:11:42 +0900

    inflearn day 2 반복문

---
+++ 인프런 실습/inflearn02.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void){
+    
+    // 증감  ++
+    // int a = 10;
+    // printf("a는 %d\n",a);
+    // a++;
+    // printf("a는 %d\n",a);
+
+    // int b = 20;
+    // printf("b는 %d\n",++b); // 전위증감
+    // printf("b는 %d\n",b++); // 후위증감
+    // printf("b는 %d\n",b);
+
+    // 반복문
+    // for, while, do while
+    // for(선언; 조건; 증감)
+    for (int i = 1; i<=10; i++){
+        printf("Hello World %d\n",i);
+    }
+
+    return 0;
+}
