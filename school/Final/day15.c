2587a0be984e4a93ec832d4039ae5bd321536580
Chae Haram <djvemfla2@gmail.com>
2022-06-08 17:15:25 +0900

    15주차 실습 5

--- school/Final/day15.c
+++ school/Final/day15.c
@@ -157,37 +157,63 @@ int main() {
     // 크기 10인 배열을 사용할 것
     // 낮은 첨자 위치에서부터 저장할 것
 
-    while (1){
+    // while (1){
 
-        long long num, mok, nmg;
-        int cnt = -1;
-        int complement[10] = {0,};
+    //     long long num, mok, nmg;
+    //     int cnt = -1;
+    //     int complement[10] = {0,};
 
-        printf("Input N : ");
-        scanf("%lld", &num);
+    //     printf("Input N : ");
+    //     scanf("%lld", &num);
          
-        if(num > 1.0995116e+12-1){
-            printf("Wrong Number. 0 ~ 1.0995116e+12\n");
-            continue;
-        }
+    //     if(num > 1.0995116e+12-1){
+    //         printf("Wrong Number. 0 ~ 1.0995116e+12\n");
+    //         continue;
+    //     }
 
-        mok = num;
+    //     mok = num;
 
-        do{
-            cnt++;
-            mok = num / 16;
-            nmg = num - mok * 16;
+    //     do{
+    //         cnt++;
+    //         mok = num / 16;
+    //         nmg = num - mok * 16;
     
-            complement[cnt] = nmg;
-            num = mok;
-        }while (mok!=0);
+    //         complement[cnt] = nmg;
+    //         num = mok;
+    //     }while (mok!=0);
 
-        for(int i=cnt, k; i>=0; i--){
-            k = complement[i];
-            if(k >= 10 && k<= 15)
-                printf("%c", k+55);
-            else
-                printf("%d ", k);
+    //     for(int i=cnt, k; i>=0; i--){
+    //         k = complement[i];
+    //         if(k >= 10 && k<= 15)
+    //             printf("%c", k+55);
+    //         else
+    //             printf("%d ", k);
+    //     }
+    //     printf("\n");
+    // }
+
+    // 실습 5.
+    // 정수의 이진 비트 스트림 구하기
+    // 양수 음수 모두 처리
+    
+    // 5-1.
+    // MSB -> LSB 방향으로 한 비트씩 LSB위치로 Shift한 후, 정수 1과 비트 AND 후 출력
+
+    while(1){
+        unsigned int num;
+        int cnt=0;
+
+        printf("Input Number (+,-): ");
+        scanf("%u",&num);
+        if (num==0)
+            break;
+
+        for(int i=31; i>=0; i--){
+            if(cnt%4==0 && cnt!=0 ){
+                printf("  ");
+            }
+            printf("%d",num >> i & 1);
+            cnt++;
         }
         printf("\n");
     }
