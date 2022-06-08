e6a421aa3548e3d3023f3fec98fa1f0d6a8ec46c
Chae Haram <djvemfla2@gmail.com>
2022-06-08 16:22:14 +0900

    15주차 실습 3

--- school/Final/day15.c
+++ school/Final/day15.c
@@ -80,18 +80,57 @@ int main() {
 
     // 실습 2-2
     // 큰 첨자 위치에서부터 이진수를 저장
+    // while (1){
+
+    //     int num;
+    //     int mok;
+    //     int nmg;
+    //     int cnt = 10;
+    //     int complement[10] = {0,};
+
+    //     printf("Input N : ");
+    //     scanf("%d",&num);
+
+    //     if(num > 1024-1){
+    //         printf("Wrong Number. 0 ~ 1024\n");
+    //         continue;
+    //     }
+
+    //     mok = num;
+
+    //     do{
+    //         cnt--;
+    //         mok = num / 2;
+    //         nmg = num - mok * 2;
+    //         complement[cnt] = nmg;
+    //         num = mok;
+    //     }while (mok!=0); 
+
+    //     int k = cnt;
+
+    //     for(int i=k; i<10; i++){
+    //         printf("%d ",complement[i]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // 실습 3.
+    // 10진수(양의 정수)를 8진수로 변환
+    // 크기 10인 배열을 사용할 것
+    // 작은 첨자에서부터 저장할 것
+
     while (1){
 
         int num;
         int mok;
         int nmg;
-        int cnt = 10;
+        int cnt = -1;
         int complement[10] = {0,};
 
         printf("Input N : ");
         scanf("%d",&num);
-
-        if(num > 1024-1){
+         
+        if(num > 1073741824-1){
             printf("Wrong Number. 0 ~ 1024\n");
             continue;
         }
@@ -99,20 +138,18 @@ int main() {
         mok = num;
 
         do{
-            cnt--;
-            mok = num / 2;
-            nmg = num - mok * 2;
+            cnt++;
+            mok = num / 8;
+            nmg = num - mok * 8;
             complement[cnt] = nmg;
             num = mok;
-        }while (mok!=0); 
+        }while (mok!=0);
+        complement[cnt+1] = 0;
 
-        int k = cnt;
-
-        for(int i=k; i<10; i++){
+        for(int i=cnt+1; i>=0; i--){
             printf("%d ",complement[i]);
         }
         printf("\n");
     }
-
 
 }
