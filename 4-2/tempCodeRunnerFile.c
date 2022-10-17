2fcb0c0d77e264916edc3ba4ad294cdabc271152
Chae Haram <djvemfla2@gmail.com>
2022-10-18 03:16:36 +0900

    c언어 중간고사 대비

--- 4-2/tempCodeRunnerFile.c
+++ 4-2/tempCodeRunnerFile.c
@@ -1,13 +1,10 @@
-
-    // double c = 1.0;
-    // int *pa, *pb;
-    // double *pc = &c;
-    // pa = &a;
-    // pb = &b;
-    // *pa = *pa + *pb;
-    // *pb = *pa + *pb;
-    // *pc = *pc / 2.0;
-    // double result = (a+b+c) / sizeof(pc);
-    // // a = 2, b = 3, c = 0.5
-    // // sizeof(pc) = 8
-    // printf("%.6f",result);
+a
+    int *p = arr;           //b
+    const int *q = arr;     //c
+    int *r = NULL;
+    p[4]++;                 //d
+    q[4]++;                 //e o
+    p++;                    //f
+    q++;                    //g
+    arr++;                  //h o
+    arr=r;                  //i o
