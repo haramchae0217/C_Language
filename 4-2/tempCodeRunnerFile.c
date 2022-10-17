498ea886d2b4a9cc8bafdd4a13ff984775e6f842
Chae Haram <djvemfla2@gmail.com>
2022-10-18 02:26:00 +0900

    c언어 midterm 대비

--- 4-2/tempCodeRunnerFile.c
+++ 4-2/tempCodeRunnerFile.c
@@ -1,11 +1,11 @@
-] = "ABCDEFG";
-    // char str[30] = "abcd";
-    // char * ps1 = "12";
+[30] = "ABCDEFG";
+    char str[30] = "abcd";
+    char * ps1 = "12";
 
-    // strcpy(myString,"M");
-    // strcpy(myString,ps1);
-    // strcpy(myString,str);
-    // int length = strlen(myString);
-    // for(int i = 0; i < 7; i++){
-    //     printf("%c",myString[i]);
-    // }
+    strcpy(myString,"M");
+    strcpy(myString,ps1);
+    strcpy(myString,str);
+    int length = strlen(myString);
+    for(int i = 0; i < 7; i++){
+        printf("%c",myString[i]);
+    }
