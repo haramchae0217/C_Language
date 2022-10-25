33027130c3d544948cd42e22157f129289c5f2dd
Chae Haram <djvemfla2@gmail.com>
2022-10-25 12:20:29 +0900

    9주차 실습 -1

--- 4-2/week9-1.c
+++ 4-2/week9-1.c
@@ -7,6 +7,14 @@
 
 int main() {
 
+    char small, cap = 'G';
+
+    if((cap>='A') && (cap <= 'Z')) {
+        small = cap + ('a' - 'A');
+    }
+
+    printf("대문자 : %c %c",cap,'\n');
+    printf("소문자 : %c",small);
 
     return 0;
 }
