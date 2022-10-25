3203b75567702c8c92709a8cc17b3dd6e6ae9834
Chae Haram <djvemfla2@gmail.com>
2022-10-25 13:28:32 +0900

    9주차 실습 -6

--- 4-2/week9-1.c
+++ 4-2/week9-1.c
@@ -42,15 +42,39 @@ int main() {
     // printf("입력한 문자열 : %s\n", str);
 
     // ex5)
-    int num, grade;
+    // int num, grade;
 
-    printf("학번 입력 : ");
-    scanf("%d", &num);
-    getchar();
+    // printf("학번 입력 : ");
+    // scanf("%d", &num);
+    // getchar(); // 버퍼에 남아 있는 개행문자 제거
 
-    printf("학점 입력 : ");
-    grade = getchar();
-    printf("학번 : %d, 학점 : %c", num, grade);
+    // printf("학점 입력 : ");
+    // grade = getchar();
+    // printf("학번 : %d, 학점 : %c", num, grade);
+
+    // ex6)
+    // 사용자로 부터 '$' 문자를 입력할 때까지 최대 100개 이내의 문자들을 입력받아,
+    // 문자열로 만들고, 이 중 아스키 코드 값이 가장 큰 문자를 찾아 코드 값과 함께 출력
+
+    char str[101];
+    char ch;
+    char maxCh;
+    int i = 0;
+    
+    while((ch = getchar()) != EOF && (ch!= '$') && (i < sizeof(str) - 1)) {
+        str[i++] = ch;
+    }
+    str[i] = '\0';
+    maxCh = str[0];
+
+    for(int j = 1; j < (sizeof(str) - 1) && str[j] != '\0'; j++) {
+        if(maxCh < str[j]) {
+            maxCh = str[j];
+        }
+    }
+
+    printf("입력한 문자열 : %s\n", str);
+    printf("아스키 코드 값이 가장 큰 문자 : %c", maxCh);
 
 
     return 0;
