fcb772e9f251e666dc460c4fc75090deb5f8353c
Chae Haram <djvemfla2@gmail.com>
2022-10-25 12:47:00 +0900

    9주차 실습 -3

--- 4-2/week9-1.c
+++ 4-2/week9-1.c
@@ -7,19 +7,31 @@
 
 int main() {
 
-    char small, cap = 'G';
+    // ex1)
+    // char small, cap = 'G';
 
-    if((cap>='A') && (cap <= 'Z')) {
-        small = cap + ('a' - 'A');
+    // if((cap>='A') && (cap <= 'Z')) {
+    //     small = cap + ('a' - 'A');
+    // }
+
+    // printf("대문자 : %c %c",cap,'\n');
+    // printf("소문자 : %c\n",small);
+
+    // ex2)
+    // char ch1 = getc(stdin);
+    // char ch2 = getchar();
+
+    // printf("%c \n", ch1);
+
+    // ex3)
+    int res;
+    char ch;
+
+    while(1) {
+        res = scanf("%c",&ch);
+        if(res == -1) break;
+        printf("%d ", ch);
     }
-
-    printf("대문자 : %c %c",cap,'\n');
-    printf("소문자 : %c\n",small);
-
-    char ch1= getc(stdin);
-    char ch2 = getchar();
-
-    printf("%c \n", ch1);
 
     return 0;
 }
