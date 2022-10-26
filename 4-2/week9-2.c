fb16bd8d110203bcd37645626f8a3855f649b490
Chae Haram <djvemfla2@gmail.com>
2022-10-26 15:11:27 +0900

    9-2주차 실습 -2

--- 4-2/week9-2.c
+++ 4-2/week9-2.c
@@ -7,11 +7,19 @@
 
 int main() {
 
+    // ex1)
     printf("apple이 저장된 시작 주소 값 : %p\n", "apple");
     printf("두 번째 문자의 주소 값 : %p\n", "apple" + 1);
     printf("첫 번째 문자 : %c\n", *"apple");
     printf("두 번째 문자 : %c\n", *("apple" + 1));
     printf("배열로 표현한 세 번째 문자 : %c\n", "apple"[2]);
+
+    // ex2)
+    char *dessert = "apple";
+
+    printf("오늘 후식은 %s입니다.\n", dessert);
+    dessert = "banana";
+    printf("내일 후식은 %s입니다.\n", dessert);
 
     return 0;
 }
