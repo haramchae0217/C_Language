a6a064c1a5b56c8df82ea6c8028b91cec2cba2f7
Chae Haram <djvemfla2@gmail.com>
2022-10-26 15:14:27 +0900

    9-2주차 실습 -3

--- 4-2/week9-2.c
+++ 4-2/week9-2.c
@@ -8,18 +8,27 @@
 int main() {
 
     // ex1)
-    printf("apple이 저장된 시작 주소 값 : %p\n", "apple");
-    printf("두 번째 문자의 주소 값 : %p\n", "apple" + 1);
-    printf("첫 번째 문자 : %c\n", *"apple");
-    printf("두 번째 문자 : %c\n", *("apple" + 1));
-    printf("배열로 표현한 세 번째 문자 : %c\n", "apple"[2]);
+    // printf("apple이 저장된 시작 주소 값 : %p\n", "apple");
+    // printf("두 번째 문자의 주소 값 : %p\n", "apple" + 1);
+    // printf("첫 번째 문자 : %c\n", *"apple");
+    // printf("두 번째 문자 : %c\n", *("apple" + 1));
+    // printf("배열로 표현한 세 번째 문자 : %c\n", "apple"[2]);
 
     // ex2)
-    char *dessert = "apple";
+    // char *dessert = "apple";
 
-    printf("오늘 후식은 %s입니다.\n", dessert);
-    dessert = "banana";
-    printf("내일 후식은 %s입니다.\n", dessert);
+    // printf("오늘 후식은 %s입니다.\n", dessert);
+    // dessert = "banana";
+    // printf("내일 후식은 %s입니다.\n", dessert);
+
+    // ex3)
+    char str[80];
+
+    printf("문자열 입력 : ");
+    scanf("%s", str); // 배열은 이름이 곧 주소기 때문에 &연산자가 없어도 됨.
+    printf("첫 번째 단어 : %s\n", str);
+    scanf("%s",str);
+    printf("버퍼에 남아있는 두 번째 단어 : %s\n", str);
 
     return 0;
 }
