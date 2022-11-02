cdc6fcc573d721d8dcc88bc8bcb5d2db60ba56ff
Chae Haram <djvemfla2@gmail.com>
2022-11-02 15:42:28 +0900

    실습 예제 -2

--- 4-2/week10-2.c
+++ 4-2/week10-2.c
@@ -6,10 +6,22 @@ void assign(void);
 
 int main() {
 
-  auto int a = 0;
+  // 실습 -1
+  // auto int a = 0; // auto없어도 가능
 
-  assign();
-  printf("main 함수 a : %d\n", a);
+  // assign();
+  // printf("main 함수 a : %d\n", a);
+
+  // 실습 -2
+  int a = 10, b = 20;
+  printf("교환 전의 a와 b의 값 : %d, %d\n", a, b);
+  {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+  }
+  printf("교환 후의 a와 b의 값 : %d, %d", a, b);
 
   return 0;
 }
