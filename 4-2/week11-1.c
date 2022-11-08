2fc60458287e7510456b53db54935799038d9b31
Chae Haram <djvemfla2@gmail.com>
2022-11-08 13:03:03 +0900

    실습 -5

--- 4-2/week11-1.c
+++ 4-2/week11-1.c
@@ -24,12 +24,49 @@ int main() {
   // }
 
   // 실습 -3
-  int N;
-  printf("Input N : ");
-  scanf("%d", &N);
+  // int N;
+  // printf("Input N : ");
+  // scanf("%d", &N);
 
-  for(int i = 1; i <= N; i++) {
-    printf("%d ", add(i));
+  // for(int i = 1; i <= N; i++) {
+  //   printf("%d ", add(i));
+  // }
+
+  // 실습 -4
+  // int score[3][4]; // 3행 4열의 2차원 배열
+  // int total;
+  // double avg;
+  // int i, j;
+
+  // for(i = 0; i < 3; i++) {
+  //   printf("4과목의 점수 입력 : ");
+  //   for(j = 0; j < 4; j++) {
+  //     scanf("%d", &score[i][j]);
+  //   }
+  // }
+
+  // for(i = 0; i < 3; i++) {
+  //   total = 0;
+  //   for(j = 0; j < 4; j++) {
+  //     total += score[i][j];
+  //   }
+  //   avg = total / 4.0;
+  //   printf("총점 : %d, 평균 : %.2lf\n", total, avg);
+  // }
+
+  // 실습 -5
+  int num[3][4] = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12}
+  };
+  int i, j;
+  
+  for(i = 0; i < 3; i++) {
+    for (j = 0; j < 4; j++) {
+      printf("%5d", num[i][j]);
+    }
+    printf("\n");
   }
 
   return 0;
