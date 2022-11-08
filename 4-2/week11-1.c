ca66aa263fe1a119a20fba2c4a22efc704d5971e
Chae Haram <djvemfla2@gmail.com>
2022-11-08 12:34:15 +0900

    실습 -2

--- 4-2/week11-1.c
+++ 4-2/week11-1.c
@@ -6,17 +6,29 @@ int *sum(int a, int b);
 
 int main() {
 
-  int *resp;
-  resp = sum(10,20);
-  printf("두 정수의 합  : %d\n", *resp);
+  // 실습 -1
+  // int *resp;
+  // resp = sum(10,20);
+  // printf("두 정수의 합  : %d\n", *resp);
+
+  // 실습 -2
+  int n;
+  int sum = 0;
+  printf("Input N : ");
+  scanf("%d",&n);
+
+  for(int i = 1; i <= n; i++) {
+    sum += i;
+    printf("%d ", sum);
+  }
 
   return 0;
 }
 
 int *sum(int a, int b) {
   static int res;
+  // 주소를 리턴하기 때문에 res를 지역변수로 지정하면 매우 위험 따라서 static 지역변수로 지정해준다.
   res = a + b;
 
   return &res;
-  
 }
