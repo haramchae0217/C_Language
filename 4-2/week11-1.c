3badcf4cbd545c571dc4d56579ef8be539aeb2a3
Chae Haram <djvemfla2@gmail.com>
2022-11-08 12:37:20 +0900

    실습 -3

--- 4-2/week11-1.c
+++ 4-2/week11-1.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 int *sum(int a, int b);
+int *add(int n);
 
 int main() {
 
@@ -12,23 +13,42 @@ int main() {
   // printf("두 정수의 합  : %d\n", *resp);
 
   // 실습 -2
-  int n;
-  int sum = 0;
-  printf("Input N : ");
-  scanf("%d",&n);
+  // int n;
+  // int sum = 0;
+  // printf("Input N : ");
+  // scanf("%d",&n);
 
-  for(int i = 1; i <= n; i++) {
-    sum += i;
-    printf("%d ", sum);
-  }
+  // for(int i = 1; i <= n; i++) {
+  //   sum += i;
+  //   printf("%d ", sum);
+  // }
+
+  // 실습 -3
+  int N;
+  printf("Input N : ");
+  scanf("%d", &N);
+
+  int *resp;
+  resp = add(N);
+
 
   return 0;
 }
 
-int *sum(int a, int b) {
+int *add(int n) {
   static int res;
-  // 주소를 리턴하기 때문에 res를 지역변수로 지정하면 매우 위험 따라서 static 지역변수로 지정해준다.
-  res = a + b;
+  for(int i = 1; i <= n; i++) {
+    res += i;
+    printf("%d ", res);
+  }
 
   return &res;
 }
+
+// int *sum(int a, int b) {
+  // static int res;
+  // 주소를 리턴하기 때문에 res를 지역변수로 지정하면 매우 위험 따라서 static 지역변수로 지정해준다.
+  // res = a + b;
+
+  // return &res;
+// }
