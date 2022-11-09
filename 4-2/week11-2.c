b0f603fdc3fc8464a538249faa20e81eb6df5174
Chae Haram <djvemfla2@gmail.com>
2022-11-09 16:17:44 +0900

    실습 -4

--- 4-2/week11-2.c
+++ 4-2/week11-2.c
@@ -97,7 +97,19 @@ int main() {
 
   // // arr[0] = arr[1]; // 불가능
 
-  
+  // 실습 -4
+  int ary1[4] = {1,2,3,4};
+  int ary2[4] = {11,12,13,14};
+  int ary3[4] = {21,22,23,24};
+  int* pary[3] = {ary1, ary2, ary3};
+  int i, j;
+
+  for(i = 0; i < 3; i++) {
+    for (j = 0; j < 4; j++) {
+      printf("%5d ", pary[i][j]);
+    }
+    printf("\n");
+  }
 
   return 0;
 }
