c9278da96b88576f080fd08dd93fd108eea0a724
Chae Haram <djvemfla2@gmail.com>
2022-11-22 12:25:18 +0900

    실습 -2

--- 4-2/week13-1.c
+++ 4-2/week13-1.c
@@ -25,6 +25,24 @@ int main() {
   // free(pi);
   // free(pd);
 
+  // 실습 -2
+  int *pi;
+  int i, sum = 0;
+
+  pi = malloc(5 * sizeof(int));
+  if(pi == NULL) {
+    printf("# 메모리가 부족합니다.\n");
+    exit(1);
+  }
+
+  printf("다섯 명의 나이를 입력하세요 : ");
+  for(i = 0; i < 5; i++) {
+    scanf("%d", &pi[i]);
+    sum += pi[i];
+  }
+
+  printf("다섯 명의 평균 나이 : %.1f\n",(sum / 5.0));
+  free(pi);
 
   return 0;
 }
