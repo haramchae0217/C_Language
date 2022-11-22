087fe945a04b70b8cab61621ccaa148fe8c3ec10
Chae Haram <djvemfla2@gmail.com>
2022-11-22 12:22:24 +0900

    실습 -1

---
+++ 4-2/week13-1.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // 실습 -1
+  // int *pi;
+  // double *pd;
+
+  // pi = malloc(sizeof(int));
+  // if(pi == NULL) {
+  //   printf("# 메모리가 부족합니다.\n");
+  //   exit(1);
+  // }
+
+  // pd = malloc(sizeof(double));
+
+  // *pi = 10;
+  // *pd = 3.4;
+
+  // printf("정수형으로 사용 : %d\n", *pi);
+  // printf("실수형으로 사용 : %.1f\n", *pd);
+
+  // free(pi);
+  // free(pd);
+
+
+  return 0;
+}
