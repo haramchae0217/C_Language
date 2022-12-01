3eef2264c4a84491742d9a030001462211e384f5
Chae Haram <djvemfla2@gmail.com>
2022-12-01 09:58:44 +0900

    실습 -3

--- 4-2/week14-2.c
+++ 4-2/week14-2.c
@@ -70,15 +70,15 @@ int main() {
   // }
 
   // 실습 -3
-  FILE *fp;
+  // FILE *fp;
 
-  fp = fopen("a.txt", "r");
-  if(fp == NULL) {
-    printf("파일이 열리지 않았습니다.\n");
-    return 1;
-  }
-  printf("파일이 열렸습니다.\n");
-  fclose(fp);
+  // fp = fopen("a.txt", "r");
+  // if(fp == NULL) {
+  //   printf("파일이 열리지 않았습니다.\n");
+  //   return 1;
+  // }
+  // printf("파일이 열렸습니다.\n");
+  // fclose(fp);
 
   return 0;
 }
