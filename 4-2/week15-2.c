647e36f605ba71748fa4e00531f2c6cc964b626b
Chae Haram <djvemfla2@gmail.com>
2022-12-07 15:18:57 +0900

    실습 -1

---
+++ 4-2/week15-2.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define SUM(a,b) ((a) + (b))
+#define MUL(a,b) ((a) * (b))
+
+int main() {
+
+  // 실습 -1
+  // int a = 10, b = 20;
+  // int x = 30, y = 40;
+  // int res;
+
+  // printf("a + b = %d\n", SUM(a, b));
+  // printf("x + y = %d\n", SUM(x, y));
+  // res = 30 / MUL(2, 5); // MUL(2, 5)가 먼저 실행
+  // printf("res : %d\n", res);
+
+  return 0;
+}
