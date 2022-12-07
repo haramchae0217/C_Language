b25e4456ba797d50113a9eb8f2eca50de5360759
Chae Haram <djvemfla2@gmail.com>
2022-12-07 15:25:37 +0900

    실습 -3

--- 4-2/week15-2.c
+++ 4-2/week15-2.c
@@ -5,6 +5,26 @@
 #define SUM(a,b) ((a) + (b))
 #define MUL(a,b) ((a) * (b))
 
+#define PRINT_EXPR(x) printf(#x " = %d\n", x)
+#define NAME_CAT(x, y) (x ## y)
+
+#define VER 7
+#define BIT16
+
+#pragma pack(push, 1)
+
+typedef struct {
+  char ch;
+  int in;
+} Sample1;
+
+#pragma pack(pop)
+
+typedef struct {
+  char ch;
+  int in;
+} Sample2;
+
 int main() {
 
   // 실습 -1
@@ -16,6 +36,29 @@ int main() {
   // printf("x + y = %d\n", SUM(x, y));
   // res = 30 / MUL(2, 5); // MUL(2, 5)가 먼저 실행
   // printf("res : %d\n", res);
+
+  // 실습 -2
+  // int a1, a2;
+
+  // NAME_CAT(a, 1) = 10;
+  // NAME_CAT(a, 2) = 20;
+  // PRINT_EXPR(a1 + a2);
+  // PRINT_EXPR(a2 - a1);
+
+  // 실습 -3
+  // int max;
+
+  // #if VER >= 6
+  //   printf("버전 %d 입니다.\n", VER);
+  // #endif
+
+  // #ifdef BIT16
+  //   max = 32767;
+  // #else
+  //   max = 2147483647;
+  // #endif
+
+  //   printf("int형 변수의 최댓값 : %d\n", max);
 
   return 0;
 }
